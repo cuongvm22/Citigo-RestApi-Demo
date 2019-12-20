a340bc2bb6684f9448ace37f1a7b5985a3256599
cuongvm <vumanhcuong_t61@hus.edu.vn>
2019-12-20 18:28:46 +0700

    v1.3 return 3 best price

--- product/models.py
+++ product/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Product(models.Model):
-<<<<<<< HEAD
 	title = models.CharField(max_length=200)
 	name = models.CharField(max_length=200)
 	rating = models.IntegerField()
@@ -26,15 +25,4 @@ class Contain(models.Model):
 
 	def __str__(self):
 		return '{} {} {}'.format(self.storeId, self.productId, self.price)
-		
-=======
-	idProduct = models.IntegerField(default = 0)
-	title = models.CharField(max_length=200)
-	name = models.CharField(max_length=200)
-	author = models.CharField(max_length=200)
-	rating = models.IntegerField()
-	quantity = models.IntegerField()
-	price = models.IntegerField()
-	thumburl = models.CharField(max_length=200)
-	infourl = models.CharField(max_length=200)
->>>>>>> 954d86afb9da883409ef465c633a0b6193ab59c7
+
